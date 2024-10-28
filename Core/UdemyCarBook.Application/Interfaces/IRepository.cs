b09567d8a73660f2a7d546463bceddfc546b1697
Yunus Yesilcimen <byunusyesilcimen@gmail.com>
2024-10-28 22:37:16 +0300

    CQRS

--- Core/UdemyCarBook.Application/Interfaces/IRepository.cs
+++ Core/UdemyCarBook.Application/Interfaces/IRepository.cs
@@ -8,7 +8,7 @@ namespace UdemyCarBook.Application.Interfaces
 {
     public interface IRepository<T> where T : class
     {
-        Task<List<T>> GettAllAsync();
+        Task<List<T>> GetAllAsync();
         Task<T> GetByIdAsync(int id);
         Task CreateAsync(T entity);
         Task UpdateAsync(T entity);
